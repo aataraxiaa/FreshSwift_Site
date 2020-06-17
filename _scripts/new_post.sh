b5892cc2c67cc495f71a4c42fbed0f2310bc3629
Pete Smith <peadar81@gmail.com>
2020-06-17 12:10:42 +0100

    Fix scipt, remove newline character

--- _scripts/new_post.sh
+++ _scripts/new_post.sh
@@ -4,8 +4,8 @@
 cd ~/Code/FreshSwift_Site/_templates
 
 # Create file name from date and post date (different formats)
-fileName=$(date +"%d-%m-%y")
-postDate=$(date +"%Y-%m-%d")
+fileName=$(date +"%d-%m-%y-%H:%M:%S")
+postDate=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Copy Post Template, using date as the copied name
 cp post_template.md $fileName.md
@@ -17,5 +17,5 @@ echo --- >> $fileName.md
 # Move file to site content directory
 mv $fileName.md ~/Code/FreshSwift_Site/content/post/$fileName.md
 
-#  Pass file as output for next workflow action
-echo ~/Code/FreshSwift_Site/content/post/$fileName.md
+#  Pass file as output for next workflow action, removing newline characters
+echo -n ~/Code/FreshSwift_Site/content/post/$fileName.md
