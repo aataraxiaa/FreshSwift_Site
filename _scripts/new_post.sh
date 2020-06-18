7b12dfbb2ab9b15614bf857cc450a2e049f4659f
Pete Smith <peadar81@gmail.com>
2020-06-18 11:33:04 +0100

    Remove test post, update script

--- _scripts/new_post.sh
+++ _scripts/new_post.sh
@@ -3,19 +3,21 @@
 # CD to directory
 cd ~/Code/FreshSwift_Site/_templates
 
-# Create file name from date and post date (different formats)
+# Create file name from date
 fileName=$(date +"%d-%m-%y-%H:%M:%S")
-postDate=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Create post date - NOTE: Date only, specifying time does not work
+postDate=$(date +"%Y-%m-%d")
 
 # Copy Post Template, using date as the copied name
 cp post_template.md $fileName.md
 
 # Output date and ending delimiter to new file
 echo "date: \"$postDate\"" >> $fileName.md
-echo --- >> $fileName.md
+echo -n --- >> $fileName.md
 
 # Move file to site content directory
 mv $fileName.md ~/Code/FreshSwift_Site/content/post/$fileName.md
 
-#  Pass file as output for next workflow action, removing newline characters
-echo -n ~/Code/FreshSwift_Site/content/post/$fileName.md
+#  Pass file as output for next workflow action
+echo -n /Users/psmith/Code/FreshSwift_Site/content/post/$fileName.md
